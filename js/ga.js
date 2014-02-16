5ac8df6239071c500be6ad2621beab52878dd61c
ribao wei <wrb302@gmail.com>
2014-02-15 17:37:13 -0800

    updated ga tracker id

--- js/ga.js
+++ js/ga.js
@@ -3,5 +3,5 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-6455503-4', 'thecoolwei.com');
+  ga('create', 'UA-33544980-1', 'thecoolwei.com');
   ga('send', 'pageview');
